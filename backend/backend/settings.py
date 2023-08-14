a70d7829f7b42f322fb49b91696b06361c30f83b
Павел Леонтьев <fionit-2007@mail.ru>
2023-08-14 20:23:01 +0300

    Добавил в админку users Subscription модель

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,7 +107,7 @@ MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'users.User'
 
 
 REST_FRAMEWORK = {
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,2 +1,45 @@
+from typing import Tuple
 from django.contrib import admin
 
+from users.models import User, Subscription
+
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display: Tuple[str] = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+        'password',
+    )
+    search_fields: Tuple[str] = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+    )
+    list_filter: Tuple[str] = (
+        'username',
+        'email',
+    )
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display: Tuple[str] = (
+        'user',
+        'author',
+    )
+    search_fields: Tuple[str] = (
+        'user',
+        'author',
+    )
+    list_filter: Tuple[str] = (
+        'user',
+        'author',
+    )
+    empty_value_display = '-пусто-'
