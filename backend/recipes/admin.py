1083ae1e60bd36f52ac5388e5ea96a07a7cc2527
Павел Леонтьев <fionit-2007@mail.ru>
2023-08-14 03:54:01 +0300

    Созданы модели Recipe, Ingredient, Tag. Создана связующая модель RecipeIngredient для указания количества ингредиента в рецепте

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag, Ingredient, Recipe
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'color')
+    search_fields = ('name',)
+    list_editable = ('color', 'slug', 'name')
+    prepopulated_fields = {'slug': ('name',)}
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,18 +1,147 @@
 from datetime import datetime
+from typing import List
 
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
+from users.models import User
 
-#from users.models import User
+
+class Ingredient(models.Model):
+    """Модель ингредиентов"""
+
+    name = models.CharField(
+        'Название ингредиента',
+        max_length=200,
+        blank=False,
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=200,
+        blank=False,
+    )
+
+    class Meta:
+        ordering: List[str] = ['-id']
+        verbose_name: str = 'ингредиент'
+        verbose_name_plural: str = 'ингредиенты'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Tag(models.Model):
+    """Модель тэгов"""
+
+    name = models.CharField(
+        'Название тэга',
+        max_length=200,
+        blank=False,
+    )
+    color = models.CharField(
+        'Цвет тэга в HEX',
+        max_length=20,
+        blank=False,
+        unique=True,
+    )
+    slug = models.SlugField(
+        'Слаг тэга',
+        max_length=100,
+        blank=False,
+        unique=True,
+    )
+
+    class Meta:
+        ordering: List[str] = ['-id']
+        verbose_name: str = 'тэг'
+        verbose_name_plural: str = 'тэги'
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Recipe(models.Model):
-    # author = models.ForeignKey(
-    #     'users.User',
-    # )
-    title = models.CharField(
+    """Модель рецептов"""
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор рецепта',
+    )
+    name = models.CharField(
+        'Название рецепта',
         max_length=200,
+        blank=False,
     )
     image = models.ImageField(
-        upload_to='recipes/',
+        'Изображение рецепта',
+        upload_to='recipes_images/',
+        blank=False,
     )
+    text = models.TextField(
+        'Описание рецепта',
+        blank=True,
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name='Ингредиенты',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тэги',
+        related_name='recipes',
+    )
+    cooking_time = models.PositiveIntegerField(
+        'Время приготовления в минутах',
+        validators=[
+            MinValueValidator(1),
+            'Время приготовления должно быть больше 0',
+        ],
+    )
+
+    class Meta:
+        ordering: List[str] = ['-id']
+        verbose_name: str = 'рецепт'
+        verbose_name_plural: str = 'рецепты'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    """Модель для связи ингредиентов в рецептах с указанием количества"""
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients',
+        verbose_name='Рецепты',
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients',
+        verbose_name='Связанные ингредиенты',
+    )
+    amount = models.PositiveSmallIntegerField(
+        'Количество ингредиента',
+        validators=[
+            MinValueValidator(1),
+            'Количество ингредиентов должно быть больше 0',
+        ],
+    )
+
+    class Meta:
+        ordering: List[str] = ['-id']
+        verbose_name: str = 'Ингредиент рецепта'
+        verbose_name_plural: str = 'Ингредиенты рецептов'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('recipe', 'ingredient'),
+                name='unique_recipe_ingredient',
+            )
+        )
+
+    def __str__(self) -> str:
+        return self.ingredient.name
